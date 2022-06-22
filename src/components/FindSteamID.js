6c83643e32bbb31d9a69dbb900481f6b210db432
StarlightStingray <somajstingray@gmail.com>
2022-06-21 18:23:00 -0700

    Add Steam Search by profile URL and vanity URL.

--- src/components/FindSteamID.js
+++ src/components/FindSteamID.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-function FindSteamID(props) {
-	const [steamIDSearch, setSteamIDSearch] = useState([]);
+function FindSteamID(profileURL) {
+	function getUserID(profileURL) {
+		let urlArr = profileURL.split('/');
+		let profileOrID = urlArr[3];
+		let id = null;
+
+		if (profileOrID === 'profiles') {
+			id = urlArr[4];
+		} else {
+			fetch(
+				`http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${STEAMP_API_KEY}&vanityurl=${urlArr[4]}`
+			)
+				.then((res) => res.json())
+				.then((res) => {
+					if (res.success !== 1) {
+						throw 'Failed to resolve Steam profile URL';
+					}
+					console.log(res);
+					id = res.steamid;
+				})
+				.catch(console.error);
+		}
+		return id;
+	}
 
 	useEffect(() => {});
 
